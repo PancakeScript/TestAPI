065627ece8e906fdc25da92f52ff25a932abb998
Ny <nymendrikaantsa@gmail.com>
2026-04-27 05:34:32 +0300

    remove frontend

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const cors = require ('cors');
+
+app.use(cors());
+app.get('/api/message', (req, res) =>{
+    res.json({message :"Mon API fonctionne!"});
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log("serveur lancé");
+})
